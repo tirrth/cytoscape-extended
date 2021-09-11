6453aa7cd0976094b230f5d197f509cb7491753a
Tirth Patel <73392387+tirrth@users.noreply.github.com>
2021-09-11 13:39:34 +0530

    wip: mocha test intialization

--- test/test.js
+++ test/test.js
@@ -1,43 +1,147 @@
-const expect = chai.expect;
-mocha.setup({ ui: "bdd" });
+// const expect = chai.expect;
+// mocha.setup({ ui: "bdd", cleanReferencesAfterRun: true, fullTrace: true });
+// const test_data = get_random_data();
+
+// async function testLoadGraph() {
+//   return new Promise((_, reject) => {
+//     mocha.run();
+//     const test = (execute) => {
+//       try {
+//         execute();
+//       } catch (err) {
+//         reject(new Error(err));
+//       }
+//     };
+
+//     after(() => {
+//       setTimeout(() => {
+//         mocha.dispose();
+//       }, 5000);
+//     });
+
+//     describe(".loadGraph()", function () {
+//       let test_data;
+//       this.timeout(5000);
+//       beforeEach(() => (test_data = get_random_data()));
+
+//       it("should not load the same graph as before", async function () {
+//         await document.loadGraph(test_data);
+//         const response = await document.loadGraph(test_data);
+//         test(() => expect(response).to.be.false);
+//       });
+
+//       it("should animate the graph", async function () {
+//         const response = await document.loadGraph(test_data);
+//         test(() => expect(response).to.be.true);
+//       });
+//     });
+//   });
+// }
+
+// async function testScrollZoom() {
+//   // resetTests(mocha.suite);
+//   // mocha.run();
+
+//   return new Promise((_, reject) => {
+//     mocha.run((failures) => reject(failures));
+//     const test = (execute) => {
+//       try {
+//         execute();
+//       } catch (err) {
+//         reject(new Error(err));
+//         throw err;
+//       }
+//     };
+
+//     describe(".scrollZoom()", function () {
+//       let test_data;
+//       this.timeout(5000);
+//       beforeEach(() => (test_data = get_random_data()));
+
+//       it("should not load the same graph as before", async function () {
+//         await document.loadGraph(test_data);
+//         const response = await document.loadGraph(test_data);
+//         expect(response).to.be.false;
+//       });
+
+//       it("should animate the graph", async function () {
+//         const response = await document.loadGraph(test_data);
+//         expect(response).to.be.true;
+//       });
+//     });
+//   });
+// }
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   document
+//     .testLoadGraph()
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log("Err: ", err));
+// });
+
+// document.testLoadGraph = testLoadGraph;
+// document.testScrollZoom = testScrollZoom;
+
 const test_data = get_random_data();
+const expect = chai.expect;
+mocha.setup({ ui: "bdd", fullTrace: true, globals: ["CY"] });
 
-async function runTest() {
-  return new Promise((_, reject) => {
-    const test = (execute) => {
-      try {
-        execute();
-      } catch (err) {
-        console.log(err);
-        reject(new Error(err));
-        throw err;
-      }
-    };
+function filterTestData(data) {
+  const resp = {
+    async: data.async,
+    body: data.body,
+    duration: data.duration,
+    err: data.err,
+    file: data.file,
+    pending: data.pending,
+    state: data.state,
+    sync: data.sync,
+    timedOut: data.timedOut,
+    timer: data.timer,
+    title: data.title,
+    type: data.type,
+  };
+  return resp;
+}
 
-    describe("Cytoscape", function () {
-      describe(".loadGraph()", function () {
-        let test_data;
-        this.timeout(5000);
-        beforeEach(() => (test_data = get_random_data()));
+async function runTests() {
+  mocha.run();
+  return new Promise((resolve, _) => {
+    describe(".loadGraph()", function () {
+      let test_data;
+      this.timeout(5000);
 
-        it("should not load the same graph as before", async function () {
-          await document.loadGraph(test_data);
-          const response = await document.loadGraph(test_data);
-          test(() => expect(response).to.be.false);
-        });
+      beforeEach(() => (test_data = get_random_data()));
 
-        it("should animate the graph", async function () {
-          const response = await document.loadGraph(test_data);
-          test(() => expect(response).to.be.true);
-        });
+      after(function () {
+        const { tests, title } = this.test.parent;
+        const response = {
+          title,
+          tests: tests.map(filterTestData),
+          get duration() {
+            return this.tests.reduce((acc, curr) => acc + curr.duration, 0);
+          },
+        };
+        resolve(response);
+      });
+
+      it("should not load the same graph as before", async function () {
+        await document.loadGraph(test_data);
+        const response = await document.loadGraph(test_data);
+        expect(response).to.be.true;
+      });
+
+      it("should animate the graph", async function () {
+        const response = await document.loadGraph(test_data);
+        expect(response).to.be.true;
       });
     });
   });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  document.runTest();
-  mocha.run();
+document.addEventListener("DOMContentLoaded", async () => {
+  const resp = await document.runTests();
+  console.log(resp);
 });
 
-document.runTest = runTest;
+document.runTests = runTests;
